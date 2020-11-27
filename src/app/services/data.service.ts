b945e81dd098b9281a4e28be59a66793fb881d94
Rawan Abou Dehn <r.aboudehn@student.fontys.nl>
2020-11-27 21:01:04 +0100

    Go to favourite recipe

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -12,36 +12,6 @@ export class DataService {
   info = new BehaviorSubject('information');
   http: HttpClient;
 
-
-  // intercept(req: HttpRequest<any>, next: HttpHandler) {
-  //   const started = Date.now();
-  //   let ok: string;
-
-  //   // extend server response observable with logging
-  //   return next.handle(req)
-  //     .pipe(
-  //       tap(
-  //         // Succeeds when there is a response; ignore other events
-  //         event => ok = event instanceof HttpResponse ? 'succeeded' : '',
-  //         // Operation failed; error is an HttpErrorResponse
-  //         error => ok = 'failed'
-  //       ),
-  //       // Log when response observable either completes or errors
-  //       // finalize(() => {
-  //       //   const elapsed = Date.now() - started;
-  //       //   const msg = `${req.method} "${req.urlWithParams}"
-  //       //      ${ok} in ${elapsed} ms.`;
-  //       //   this.messenger.add(msg);
-  //       // })
-  //     );
-  // }
-
-  // Fixes Unsupported Media Type
-  // httpOptions = {
-  //   headers: new HttpHeaders({'Content-Type': 'application/json'})
-  // }
-
-
   constructor(private url: string, http: HttpClient) {
     this.http = http;
    }
